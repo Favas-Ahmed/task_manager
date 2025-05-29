7277a58433c97ba1e0e1c6ed97ebc687cc34b324
Favas-Ahmed <favasahmedcodes@gmail.com>
2025-05-29 15:57:05 +0530

    Update urls.py

--- regapp/urls.py
+++ regapp/urls.py
@@ -6,5 +6,7 @@ app_name = 'regapp'
 urlpatterns = [
     path('register', views.register, name='register'),
     path('login/', views.login_vew, name='login'),
+    path('logout', views.logout, name="logout"),
+
 
 ]
